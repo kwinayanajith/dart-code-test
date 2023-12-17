fc05eaf6eb38df15b8e4c592d1a6ae8d47f2ab80
Ishan Nayanajith <92754433+kwinayanajith@users.noreply.github.com>
2023-12-17 22:59:25 +0530

    dart fun

---
+++ darttest/lib/ControlFlow.dart
@@ -0,0 +1,23 @@
+// import 'dart:io';
+//
+// void main() {
+//   // Prompt the user to enter a number
+//   print('Enter a number:');
+//
+//   // Read the input as a string and convert it to an integer
+//   String userInput = stdin.readLineSync()!;
+//   int number = int.tryParse(userInput) ?? 0;
+//
+//   // Check if the number is even or odd
+//   if (isEven(number)) {
+//     print('$number is an even number.');
+//   } else {
+//     print('$number is an odd number.');
+//   }
+// }
+//
+// bool isEven(int number) {
+//   // Check if the number is even
+//   return number % 2 == 0;
+// }
+
